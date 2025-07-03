c2ba0b01d391876d24f5895b65b1ed8c4c2dbdff
= <=>
2025-07-03 08:01:49 -0300

    Client implemented for multiple files and server response
    Implementei o servidor para que funcione para caso o servidor responda múltiplos servidores e também fiz de uma forma que funcione para caso o cliente queira enviar múltiplos arquivos, por meio da criação de um protocolo próprio com cabeçalho podendo ser BEGIN, NUM_PACOTES, NOME ou BEGIN, ID, NOME e CHUNK, ID, INDICE, DATA

--- UDP - Cliente.py
+++ UDP - Cliente.py
@@ -5,8 +5,11 @@ import os
 import math
 
 BUFFER_SIZE = 1024
-DATA_SIZE = 1020
+HEADER_SIZE = 8
+DATA_SIZE = BUFFER_SIZE - HEADER_SIZE
 SERVER_ADDRESS = ('localhost', 12345)
+
+# --------------------------------- Obtendo Arquivo ------------------------------
 
 # Verifica se o nome do arquivo foi passado como argumento
 if len(sys.argv) != 2:
@@ -27,40 +30,47 @@ if not os.path.exists(arquivo_path):
 if not os.path.exists('Cliente'):
     os.makedirs('Cliente')
 
+# --------------------------- Comunicação ------------------------------------
+
+# Função bloqueante para envio de pacote ao servidor
+# Ele fica tentando enviar o pacote ao servidor até um momento em que o servidor
+# Responde com um pacote de reconhecimento, o servidor tem até 2 segundos para responder
+# Esse pacote, caso contrário ele dará timeout e o cliente reenviará o pacote
+def send_blocking(cliente, pacote):
+    while True:
+        cliente.sendto(pacote, SERVER_ADDRESS) # Envia pacote para o servidor
+        try:
+            # Sai do loop apenas se conseguir receber um pacote de reconhecimento
+            # Do servidor indicando que recebeu o pacote
+            data, _ = cliente.recvfrom(BUFFER_SIZE) 
+            break
+        except socket.timeout: # Exceção quando ocorre o timeout
+            print(f"Nao foi possivel enviar o pacote ao servidor\n")
+
+    return data # Retornando pacote de reconhecimento
+
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)              # Cria o socket UDP
 cliente.settimeout(2.0)                                                 # Define o timeout do socket para 2 segundos
-
-# Envia nome do arquivo
-cliente.sendto(ARQUIVO_LOCAL.encode(), SERVER_ADDRESS)
 
 # Envia o arquivo
 with open(arquivo_path, 'rb') as f:
     max_pacotes = math.ceil(os.path.gesize(arquivo_path) / DATA_SIZE)   # Calcula o número máximo de pacotes
     num_pacote = 0
 
-    while True:
-        pacote = struct.pack('>5sI', b'BEGIN', max_pacotes)             # Cria o pacote de início com o nome do arquivo e número máximo de pacotes
-        cliente.sendto(pacote, SERVER_ADDRESS) 
-
-        try:
-            data, _ = cliente.recvfrom(BUFFER_SIZE)                    
-            id = struct.unpack('>I', data)                              # Recebe o ID da sessão do servidor
-            break
-        except socket.timeout:
-            print(f"Nao foi possivel iniciar a comunicacao com o servidor\n")
+    pacote = struct.pack('>5sI', b'BEGIN', max_pacotes)             # Cria o pacote de início com o nome do arquivo e número máximo de pacotes
+    pacote += ARQUIVO_LOCAL.encode() # Adiciona o nome do arquivo
+    id = struct.unpack('>I', send_blocking(cliente=cliente, pacote=pacote)) # Obtendo o ID
 
     while (dados := f.read(DATA_SIZE)):                                 
-        while True:
-            cabecalho = struct.pack('>I', id) 
-            pacote = cabecalho + dados                                  # Cria o pacote com o ID da sessão e os dados do arquivo
-            cliente.sendto(pacote, SERVER_ADDRESS)
-            
-            try:
-                ack, _ = cliente.recvfrom(1)                            # Espera pela confirmação do servidor
-                break
-            except socket.timeout:
-                print(f"Nao foi possivel enviar o pacote {num_pacote}, reenviando\n")
+        cabecalho = struct.pack('>5s2I', b'CHUNK', id, num_pacote) 
+        pacote = cabecalho + dados                                  # Cria o pacote com o ID da sessão e os dados do arquivo
 
+        # Garante que apenas envie o proximo pacote se for o ID da sessao atual
+        while True:
+            ack_id = struct.unpack('>I', send_blocking(cliente=cliente, pacote=pacote))
+            if (ack_id == id):
+                break
+        
         print(f"Enviado pacote {num_pacote}\n")
         num_pacote += 1
 
@@ -70,16 +80,29 @@ print("Esperando pacotes")
 
 cliente.settimeout(None)                                                # Remove o temporizador para receber os pacotes
 
-for i in range(max_pacotes):                                            # Recebe os pacotes numerados
-    data, _ = cliente.recvfrom(BUFFER_SIZE)
-    session_id = struct.unpack('>I', data[:4])
-    if session_id != id:
-        continue                                                        # Verifica se o ID da sessão corresponde ao esperado
-    pacotes[i] = data[4:]
-    cliente.sendto(b'\x01', SERVER_ADDRESS)                             # Envia confirmação de recebimento do pacote
+num_pacote = 0
+
+# Garante o recebimento dos dados e garante que se o pacote de reconhecimento for perdido na transmissao
+# Como o servidor renviará o mesmo pacote msm o cliente tendo recebido o cliente apenas reenviará o pacote de reconhecimento
+# Garantindo que os dois não saim de sincronia
+while True:
+    data, _ = cliente.recvfrom(BUFFER_SIZE) # Recebe pacote do servidor
+    cabecalho, sessao_id = struct.unpack('>5sI', data[:9]) # Lê cabecalho
+    if (cabecalho == b'CHUNK' and sessao_id == id): # Verifica se o servidor ja esta enviando os pacotes
+        indice = struct.unpack('>I', data[9:13])
+        if (indice == num_pacote):
+            pacotes[num_pacote] = data[13:]
+            num_pacote += 1
+    elif (cabecalho == b'BEGIN' and sessao_id == id): # Verifica se o servidor iniciou a comunicao
+        novo_nome = str(data[9:].decode())
+
+    cliente.sendto(sessao_id, SERVER_ADDRESS) # Enviado pacote de reconhecimento
+
+    if (num_pacote >= max_pacotes):
+        break
 
 # Reordena os pacotes e salva o arquivo devolvido
-arquivo_retorno = os.path.join('Cliente', 'devolvido_' + ARQUIVO_LOCAL)
+arquivo_retorno = os.path.join('Cliente', novo_nome)
 with open(arquivo_retorno, 'wb') as f:
     for i in range(max_pacotes):
         f.write(pacotes[i])
